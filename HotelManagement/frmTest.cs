abf087d3123a8f0aa5f55e4013dcd536bc233874
behzadp1996 <Behzad.afb2012@yahoo.com>
2022-01-04 08:59:10 +0330

    ModuleServices - All Methods Implemented

--- HotelManagement/frmTest.cs
+++ HotelManagement/frmTest.cs
@@ -18,51 +18,17 @@ namespace HotelManagement
         {
             InitializeComponent();
 
-            //Employee employee = new Employee()
-            //{
-            //    ActID = 2029,
-            //    BranchID = 1005,
-            //    HireDate = DateTime.Now,
-            //    Education = "Bsc",
-            //    Salary = 150000
-            //};
-
-            //EmployeeService employeeService = new EmployeeService();
-            //var res = employeeService.InsertEmployee(employee);
-
-            //var res2 = employeeService.GetEmployee(16);
-            //var res3 = employeeService.GetEmployee(24,1);
-
-            //employee.ID = employeeService.LastInsertedId;
-            //employee.Education = "PHD";
-            //var res4 = employeeService.UpdateEmployee(employee);
-
-            //var res5 = employeeService.DeleteEmployee(employeeService.LastInsertedId);
-
-            //Actor actor = new Actor()
-            //{
-            //    Address = "Sth",
-            //    Birthday = DateTime.Now,
-            //    Gender = "Male",
-            //    City = "NYC",
-            //    Email = "SSSS",
-            //    Firstname = "Behzad",
-            //    Lastname = "Sthe",
-            //    ID = 3 
-            //};
-
-            //CustomerService cs = new CustomerService();
-            //Customer c = new Customer( 5 ,actor);
-            //var res = cs.GetCustomer(21, null);
-            RoleService roleService = new RoleService();
-            
-            roleService.InsertRole(new Role()
+            ModuleService moduleService = new ModuleService();
+            moduleService.InsertModule(new Module()
             {
-                Title = "Digital Marketing"
+                Title = "Chat"
             });
-            var roles = roleService.GetAllRoles();
-            var role = roleService.GetRole(roleService.LastInsertedId);
-            var result = roleService.DeleteRole(roleService.LastInsertedId);
+            var module = moduleService.GetModule(moduleService.LastInsertedId);
+            var modules = moduleService.GetAllModules();
+            var res = moduleService.DeleteModule(moduleService.LastInsertedId);
+
+            var roleModules = moduleService.GetModules(1);
+
         }
         
     }
